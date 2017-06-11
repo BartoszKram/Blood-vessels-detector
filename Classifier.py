3b039868580268c1f7c2d8edf528d11f1e18be78
BartoszKram <19kram95@gmail.com>
2017-06-11 11:35:06 +0200

    kNN parameters tests

--- Classifier.py
+++ Classifier.py
@@ -2,26 +2,33 @@ import cv2
 import numpy as np
 import scipy.misc
 import csv
+from Hist import *
+from sklearn.neighbors import KNeighborsClassifier
+
+def construct_classifier_data():
+    make_test_samples()
 
 def make_test_samples():
-            for i in range(1,2):
-                image = scipy.misc.imread('Resources/training/images/img'+str(i))
-                mask = scipy.misc.imread('Resources/training/masks/img' + str(i)+'_mask')
-                x_dim, y_dim, c = image.shape
-                training_data = []
-                targets = []
-                for y in range (2,y_dim-3,5):
-                    for x in range (2,x_dim-3,5):
-                        sample = image[x-2:x+3,y-2:y+3]
-                        sample_mask = mask[x-2:x+3,y-2:y+3]
-                        sample_attributes = extract_attributes(sample)
-                        training_data.append(sample_attributes)
-                        if(test_sample_mask(sample_mask)):
-                            targets.append('1')
-                        else:
-                            targets.append('0')
-                save_training_to_csv(training_data,targets,i)
+    training_data = []
+    targets = []
+    for i in range(1,10):
+        image = scipy.misc.imread('Resources/training/images/img'+str(i))
+        # image = transformImage(image)
+        
+        mask = scipy.misc.imread('Resources/training/masks/img' + str(i)+'_mask')
+        x_dim, y_dim, c = image.shape
 
+        for y in range (13,y_dim-13,2):
+            for x in range (13,x_dim-13,2):
+                sample = image[x-13:x+13,y-13:y+13]
+                sample_mask = mask[x-13:x+13,y-13:y+13]
+                sample_attributes = extract_attributes(sample)
+                training_data.append(sample_attributes)
+                if(test_sample_mask(sample_mask)):
+                    targets.append('1')
+                else:
+                    targets.append('0')
+    save_training_to_csv(training_data,targets)
 
 def extract_attributes(sample):
     attributes = []
@@ -34,23 +41,17 @@ def extract_attributes(sample):
 def test_sample_mask(sample_mask):
     x_dim, y_dim = sample_mask.shape
     val=0
-    for y in range(int(y_dim / 2) - 2, int(y_dim / 2) + 2):
-        for x in range(int(x_dim/2)-2, int(x_dim/2)+2):
+    for y in range(int(y_dim / 2) - 1, int(y_dim / 2) + 1):
+        for x in range(int(x_dim/2)-1, int(x_dim/2)+1):
             val+= sample_mask[x,y]
-    if(val>6):
+    if(val>=6):
         return True
     else:
         return False
 
-
-# wydobycie cech obrazów treningowych z określoną przynależnością żyłka/przestrzeń
-# nauka klasyfikatora
-# testowanie klasyfikatora na zbiorze uczącym
-# Wariancja kolorów
-
-def save_training_to_csv(attributes,targets,i):
-    with open('Resources/training/attributes'+str(i)+'.csv','w', newline='') as csvattributes:
-        with open('Resources/training/targets'+str(i)+'.csv','w', newline='') as csvtargets:
+def save_training_to_csv(attributes,targets):
+    with open('Resources/training/attributes.csv','w', newline='') as csvattributes:
+        with open('Resources/training/targets.csv','w', newline='') as csvtargets:
             a_writer = csv.writer(csvattributes, delimiter=' ',
                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
             t_writer = csv.writer(csvtargets, delimiter=' ',
@@ -59,3 +60,66 @@ def save_training_to_csv(attributes,targets,i):
                 a_writer.writerow(a_row)
             for t_row in targets:
                 t_writer.writerow(t_row)
+
+def read_training_data(mode):
+    if(mode==0):
+        attributes = []
+        with open('Resources/training/attributes.csv','r') as csvattributes:
+            a_reader = csv.reader(csvattributes, delimiter=' ', quotechar='|')
+            for row in a_reader:
+                tmp=[]
+                for e in row:
+                    tmp.append(float(e))
+                attributes.append(tmp)
+        return attributes
+    else:
+        targets = []
+        with open('Resources/training/targets.csv', 'r') as csvattributes:
+            a_reader = csv.reader(csvattributes, delimiter=' ', quotechar='|')
+            for row in a_reader:
+                for e in row:
+                    targets.append(int(e))
+        return targets
+
+
+def make_samples(image):
+    attributes=[]
+    x_dim,y_dim,c = image.shape
+    for y in range(13, y_dim - 13,2):
+        for x in range(13, x_dim - 13,2):
+            sample = image[x - 13:x + 13, y - 13:y + 13]
+            sample_attributes = extract_attributes(sample)
+            sample_attributes.append(str(x))
+            sample_attributes.append(str(y))
+            attributes.append(sample_attributes)
+    save_attributes(attributes)
+
+def save_attributes(attributes):
+    with open('Results/attributes.csv','w', newline='') as csvattributes:
+        a_writer = csv.writer(csvattributes, delimiter=' ',
+                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for a_row in attributes:
+            a_writer.writerow(a_row)
+
+def read_attributes():
+    attributes = []
+    with open('Results/attributes.csv', 'r') as csvattributes:
+        a_reader = csv.reader(csvattributes, delimiter=' ', quotechar='|')
+        for row in a_reader:
+            tmp = []
+            for e in row:
+                tmp.append(float(e))
+            attributes.append(tmp)
+    return attributes
+
+
+def calculate_attributes(image):
+    make_samples(image)
+    return read_attributes()
+
+
+# wydobycie cech obrazów treningowych z określoną przynależnością żyłka/przestrzeń /
+# nauka klasyfikatora /
+# testowanie klasyfikatora na zbiorze uczącym /
+#
+# Wariancja kolorów
--- main.py
+++ main.py
@@ -5,17 +5,41 @@ from Hist import *
 from Classifier import *
 
 if __name__ == '__main__':
-    img = misc.imread('Resources/healthy/data',0)
-    # red = img.copy()
-    make_test_samples()
-    # plt.subplot(1,2,1)
-    # plt.imshow(red, cmap='gray')
-    # filtered = gaussianFilter(red)
-    # transformed = transformImage(filtered)
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(filtered, cmap='gray')
-    # plt.show()
+
+    construct_classifier_data()
+    kNN_attributes = read_training_data(0)
+    targets = read_training_data(1)
+    classifier = KNeighborsClassifier(n_neighbors=3)
+    classifier.fit(kNN_attributes,targets)
+
+    image = scipy.misc.imread('Resources/test/images/img20')
+    # image = scipy.misc.imread('Resources/test/images/1')
+    # image = transformImage(image)
+
+    result = np.zeros_like(image)
+    image_attributes = calculate_attributes(image)
+    for sample in image_attributes:
+        sample_attributes = sample[:-2]
+        x = int(sample[-2])
+        y = int(sample[-1])
+        if(classifier.predict([sample_attributes])[0]):
+            result[x-1:x+1,y-1:y+1]=255
+    plt.subplot(1,2,1)
+    plt.imshow(result, cmap='gray')
+
+    # mask = scipy.misc.imread('Resources/test/masks/1')
+    mask = scipy.misc.imread('Resources/test/masks/img20_mask')
+    plt.subplot(1, 2, 2)
+    plt.imshow(mask, cmap='gray')
+    plt.show()
 
 
 # Zapis wyników
 # SK learn / orange
+
+# Możliwe parametry :
+# Momenty centralne, a nie Hu
+# Liczba sąsiadów
+# rysowanie tylko środkowego piksela, iterowanie po wszystkich
+# wielkość kwadracika (póki co 5x5)
+#
